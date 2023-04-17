fdb0dca213f278de703a9ff86d6b49d0000e2ef5
berakun <ippirifki013@gmail.com>
2023-04-17 23:22:44 +0700

    commit tahap 2 17 april

--- components/adminpage/AdminPage.vue
+++ components/adminpage/AdminPage.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="relative overflow-x-auto w-screen py-10 px-10">
+
+    <div class="mb-4">
+      <input type="text" v-model="search" placeholder="Search by product name, color, or category"
+        class="w-full px-4 py-2 text-sm text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+    </div>
+
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
@@ -9,7 +15,8 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(v, k) in users" :key="k" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
+        <tr v-for="(v, k) in filteredUsers" :key="k"
+          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
           <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
             {{ k + 1 }}
           </td>
@@ -51,6 +58,20 @@ export default {
     const response = await this.$gorilla.get('/');
     this.users = response.data.data;
     console.log(response);
+  },
+  computed: {
+    filteredUsers() {
+      if (this.search === '') return this.users;
+      else {
+        return this.users.filter(user => {
+          return Object.keys(user).some(key => {
+            if (key === 'product_name') {
+              return String(user[key]).toLowerCase().includes(this.search.toLowerCase());
+            }
+          });
+        });
+      }
+    }
   },
 };
 

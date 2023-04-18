e9ec58f447bc2862945236f7ff00c7aa06197524
berakun <ippirifki013@gmail.com>
2023-04-18 21:41:56 +0700

    commit 17 april tahap 2

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default function ({ $axios }, inject) {
   console.log($axios)
   const gorilla = $axios.create({
-    baseURL: 'http://localhost:8000/api/posts',
+    baseURL: 'http://localhost:8000/api/post/books',
     timeout: 5000
     // headers: {
     //   Authorization: `Bearer FeUOw1LkQVnbXrZ8LozPtwVlumXI8ABfANNZfJaVBWg3_tPdzw`
